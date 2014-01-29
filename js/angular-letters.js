3421096bd91f3e33791827c7cc13ecb5f4583c57
DeFUCC <fond@fondfrukt.ru>
2014-01-30 00:04:12 +0400

    Now with an Order object for every saying.
    It's needed  for future nesting of sayings. Hope it will soon emerge.

--- js/angular-letters.js
+++ js/angular-letters.js
@@ -8,19 +8,109 @@ fruitTree.controller(controllers);
 controllers.lettersCtrl = function ($scope,$firebase) {
     var baseLetters = ['A','B','C','E','H','K','M','O','P','T','X','Y'];
     var baseColors = ['c', 'cd', 'd','dd','e','f','fd','g','gd','a','ad','b'];
+
+    function Order (order, text) {
+        var baseLetters = ['A','B','C','E','H','K','M','O','P','T','X','Y'];
+        var ordr = this,
+            bit = 1,
+            availableOrders = baseLetters.concat(),
+            pluses=0, zeros=0, minuses=0;
+        var refillOrders = function () {
+            var current, result=baseLetters.concat(), order;
+
+            for (var b=0;b<bit; b++) {
+                current = result.slice(0);
+                order=0;
+                for (var i=0; i<current.length;i++) {
+
+                    for (var j=0;j<12;j++) {
+
+                        result[order++]=current[i]+baseLetters[j];
+                    }
+                }
+            }
+            bit++;
+            availableOrders = result;
+            ordr.shuffleOrders();
+        };
+        ordr.order = order || '0';
+        ordr.text = text || '';
+        ordr.color = ordr.order.substring(0,1);
+        ordr.sayings = [];
+        ordr.pretakenOrders = [];
+        ordr.freeOrders = shuffle(availableOrders);
+        ordr.shuffleOrders = function () {
+            ordr.freeOrders = shuffle(availableOrders);
+        };
+        ordr.pretakeOrder = function (order) {
+            var place = availableOrders.indexOf(order);
+               if (place>=0){
+                    var time = new Date();
+                    availableOrders.splice(place,1);
+                    ordr.pretakenOrders.push({order:order, time:time});
+                    return {order:order, time:time}
+               }
+            return false;
+        };
+        ordr.returnOrder = function (order) {
+            var place = ordr.pretakenOrders.indexOf(order);
+            if (place>=0){
+                ordr.pretakenOrders.splice(place,1);
+                availableOrders.push(order);
+            }
+        };
+        ordr.say = function (order, text) {
+            var place = availableOrders.indexOf(order);
+            if (place>=0) {
+                ordr.sayings.push({order:order, text:text});
+                ordr[order]=new Order(order,text);
+                availableOrders.splice(place,1);
+                ordr.shuffleOrders();
+                $scope.linkedText = '';   //view repair
+                if (availableOrders.length == 0) {refillOrders()};
+                return true;
+            }
+            return false;
+        };
+        ordr.rate = function (sign) {
+            switch (sign) {
+                case '+':
+                    pluses++;
+                    break;
+                case '0':
+                    zeros++;
+                    break;
+                case '-':
+                    minuses++;
+                    break;
+                default:
+                    zeros++;
+            }
+        };
+        ordr.getRating = function () {
+            return pluses - minuses;
+        };
+        ordr.getTotal = function () {
+            return pluses + minuses + zeros;
+        };
+    }
+
+    $scope.root = new Order();
+
+
     $scope.availableColors = baseColors.concat();
     $scope.bit = 1;
     $scope.currentLetters = baseLetters.concat();
     $scope.showLetters = shuffle($scope.currentLetters).slice(0,12);
     $scope.popLetter = $scope.showLetters.pop();
-    $scope.myLetters = [];
+    $scope.sayings = [];
     $scope.colors = [];
     $scope.rating = [];
     $scope.fireLetters = $firebase(new Firebase('http://fruit-tree.firebaseio.com')); //creating a firebase object
     $scope.fireLetters.$bind($scope, "remoteLetters"); //bind a $scope.remoteLetters object for saving
     $scope.saveToFireBase = function () {
         var remote = $scope.remoteLetters;
-        remote.myLetters = $scope.myLetters;
+        remote.sayings = $scope.sayings;
         remote.currentLetters = $scope.currentLetters;
         remote.bit = $scope.bit;
         remote.colors = $scope.colors;
@@ -28,7 +118,7 @@ controllers.lettersCtrl = function ($scope,$firebase) {
     };
     $scope.loadFromFireBase = function () {
         var remote = $scope.remoteLetters;
-        $scope.myLetters = remote.myLetters;
+        $scope.sayings = remote.sayings;
         $scope.currentLetters = remote.currentLetters;
         $scope.bit = remote.bit;
         $scope.colors = remote.colors;
@@ -40,7 +130,7 @@ controllers.lettersCtrl = function ($scope,$firebase) {
     $scope.takeLetter = function(letter) {
         var place = $scope.currentLetters.indexOf(letter);
         if (place >= 0) {
-            $scope.myLetters.push({letter: letter, text: $scope.linkedText});
+            $scope.sayings.push({letter: letter, text: $scope.linkedText});
             $scope.rating.push({letter:letter, pluses:0,minuses:0,zeros:0});
             $scope.currentLetters.splice(place,1);
             $scope.linkedText = '';
@@ -59,7 +149,7 @@ controllers.lettersCtrl = function ($scope,$firebase) {
     $scope.loadFromLocalStorage = function () {
        if (localStorage.length > 0) {
        $scope.currentLetters=JSON.parse(localStorage["currentLetters"]);
-       $scope.myLetters=JSON.parse(localStorage["myLetters"]);
+       $scope.sayings=JSON.parse(localStorage["sayings"]);
        $scope.bit=localStorage["bit"];
        $scope.colors=JSON.parse(localStorage["colors"]);
        $scope.rating=JSON.parse(localStorage["rating"]);
@@ -72,14 +162,14 @@ controllers.lettersCtrl = function ($scope,$firebase) {
         $scope.bit = 1;
         $scope.currentLetters = baseLetters.concat();
         $scope.shuffleLetters();
-        $scope.myLetters = [];
+        $scope.sayings = [];
         $scope.colors = [];
         $scope.rating = [];
         $scope.selectedOrder=false;
     };
 
     $scope.saveToLocalStorage = function () {
-        localStorage["myLetters"] = JSON.stringify($scope.myLetters);
+        localStorage["sayings"] = JSON.stringify($scope.sayings);
         localStorage["currentLetters"] = JSON.stringify($scope.currentLetters);
         localStorage["bit"] = $scope.bit;
         localStorage["colors"] = JSON.stringify($scope.colors);
