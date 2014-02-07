3d070a3c5ceffaf0aa22e95b3f0e4afb3e503f94
DeFUCC <fond@fondfrukt.ru>
2014-02-08 00:15:34 +0400

    All functions out.
    Out to Order.prototype. Now ready to full nesting with database ready
    structure for JSON covertation.

--- js/angular-letters.js
+++ js/angular-letters.js
@@ -8,11 +8,14 @@ fruitTree.controller(controllers);
 controllers.lettersCtrl = function ($scope,$firebase) {
     var baseLetters = ['A','B','C','E','H','K','M','O','P','T','X','Y'];
     var baseColors = ['c', 'cd', 'd','dd','e','f','fd','g','gd','a','ad','b'];
+    var boxWidth = 160;
+    var retina = window.devicePixelRatio > 1;
+    if (retina) {boxWidth=320};
 
     var fruit = document.getElementById('fruit');
     $scope.columner = function () {
         $scope.width=fruit.clientWidth;
-        $scope.colsmax=Math.floor($scope.width/160);
+        $scope.colsmax=Math.floor($scope.width/boxWidth);
         $scope.cols = $scope.colsmax;
         $scope.$apply();
     };
@@ -37,127 +40,129 @@ controllers.lettersCtrl = function ($scope,$firebase) {
 
         return arr;
     }
+
     function Order (order, type, picLink, text) {
         var baseLetters = ['A','B','C','E','H','K','M','O','P','T','X','Y'];
-        var ordr = this,
-            bit = 1,
-            pluses=0, zeros=0, minuses=0;
-        var refillOrders = function () {
-            var current, result=baseLetters.concat(), order;
-
-            for (var b=0;b<bit; b++) {
-                current = result.slice(0);
-                order=0;
-                for (var i=0; i<current.length;i++) {
-
-                    for (var j=0;j<12;j++) {
-
-                        result[order++]=current[i]+baseLetters[j];
-                    }
-                }
-            }
-            bit++;
-            ordr.availableOrders = result;
-            ordr.shuffleOrders();
-        };
-        ordr.order = order || '0';
-        ordr.type = type || '';
-        ordr.picLink = picLink || '';
-        ordr.text = text || '';
-        ordr.sayings = [];
-        ordr.availableOrders = baseLetters.concat();
-        ordr.pretaken='';
-        ordr.freeOrders = shuffle(ordr.availableOrders);
-        ordr.shuffleOrders = function () {
-            ordr.freeOrders = shuffle(ordr.availableOrders);
-        };
-        ordr.pretakeOrder = function (order) {
-                    ordr.pretaken = order;
-                    ordr.shuffleOrders();
-                    return order
-        };
-        ordr.returnOrder = function () {
-            ordr.pretaken='';
-        };
-        ordr.say = function (order, type, picLink, text) {
-            var place = ordr.availableOrders.indexOf(order);
-            if (place>=0) {
-                var time = new Date();
-                ordr.sayings.push({order:order, type:type, picLink:picLink, date:time.toLocaleDateString(), time:time.toLocaleTimeString(),  text:text});
-                ordr[order]=new Order(order,type,picLink,text);
-                ordr.availableOrders.splice(place,1);
-                ordr.pretaken='';
-                ordr.shuffleOrders();
-                $scope.linkedText = '';   //view repair
-                if (ordr.availableOrders.length == 0) {refillOrders()};
-                return true;
-            }
-            return false;
-        };
-        ordr.rate = function (sign) {
-            switch (sign) {
-                case '+':
-                    pluses++;
-                    break;
-                case '0':
-                    zeros++;
-                    break;
-                case '-':
-                    minuses++;
-                    break;
-                default:
-                    zeros++;
-            }
-        };
-        ordr.getRating = function () {
-            return pluses - minuses;
-        };
-        ordr.getTotal = function () {
-            return pluses + minuses + zeros;
-        };
-        ordr.ratingSort = function (card) {
-            var order = card.order;
-            return -ordr[order].getRating();
-        };
-        ordr.exportSayings = function () {
-            var result = {},i;
-            result.order=ordr.order;
-            result.type=ordr.type;
-            result.picLink=ordr.picLink;
-            result.text=ordr.text;
-            result.pretakenOrders=ordr.pretakenOrders;
-            result.bit=ordr.bit;
-            result.sayings=ordr.sayings;
-            result.pluses=pluses;
-            result.minuses=minuses;
-            result.zeros=zeros;
-            result.availableOrders=ordr.availableOrders;
-            for (i=0;i<ordr.sayings.length;i++) {
-                result[ordr.sayings[i].order]=ordr[ordr.sayings[i].order].exportSayings();
-            }
-            return result;
-        };
-        ordr.importSayings = function (obj) {
-            ordr.order=obj.order || '0';
-            ordr.type=obj.type || '';
-            ordr.picLink=obj.picLink || '';
-            ordr.text=obj.text || '';
-            ordr.pretakenOrders=obj.pretakenOrders || [];
-            bit=obj.bit || 1;
-            ordr.sayings=obj.sayings || [];
-            pluses=obj.pluses || 0;
-            minuses=obj.minuses || 0;
-            zeros=obj.zeros || 0;
-            ordr.availableOrders=obj.availableOrders;
-            for (var i=0;i<ordr.sayings.length;i++) {
-                ordr[ordr.sayings[i].order] = new Order();
-                ordr[ordr.sayings[i].order].importSayings(obj[obj.sayings[i].order]);
-            }
-
-        }
+        this.pluses=0;
+        this.zeros=0;
+        this.minuses=0;
+        this.order = order || '0';
+        this.type = type || '';
+        this.picLink = picLink || '';
+        this.text = text || '';
+        this.sayings = [];
+        this.bit = 1;
+        this.availableOrders = baseLetters.concat();
+        this.pretaken='';
+        this.freeOrders = shuffle(this.availableOrders);
     }
 
+Order.prototype.importSayings = function (obj) {
+    this.order=obj.order || '0';
+    this.type=obj.type || '';
+    this.picLink=obj.picLink || '';
+    this.text=obj.text || '';
+    this.pretakenOrders=obj.pretakenOrders || [];
+    this.bit=obj.bit || 1;
+    this.sayings=obj.sayings || [];
+    this.pluses=obj.pluses || 0;
+    this.minuses=obj.minuses || 0;
+    this.zeros=obj.zeros || 0;
+    this.availableOrders=obj.availableOrders;
+    this.shuffleOrders();
+    for (var i=0;i<this.sayings.length;i++) {
+        this[this.sayings[i].order] = new Order();
+        this[this.sayings[i].order].importSayings(obj[obj.sayings[i].order]);
+    }
 
+};
+Order.prototype.exportSayings = function () {
+        var result = {},i;
+        result.order=this.order;
+        result.type=this.type;
+        result.picLink=this.picLink;
+        result.text=this.text;
+        result.pretakenOrders=this.pretakenOrders;
+        result.bit=this.bit;
+        result.sayings=this.sayings;
+        result.pluses=this.pluses;
+        result.minuses=this.minuses;
+        result.zeros=this.zeros;
+        result.availableOrders=this.availableOrders;
+        for (i=0;i<this.sayings.length;i++) {
+            result[this.sayings[i].order]=this[this.sayings[i].order].exportSayings();
+        }
+        return result;
+    };
+Order.prototype.ratingSort = function (card) {
+    return $scope.root[card.order].minuses - $scope.root[card.order].pluses;
+};
+Order.prototype.getTotal = function () {
+        return this.pluses + this.minuses + this.zeros;
+    };
+Order.prototype.getRating = function () {
+    return this.pluses - this.minuses;
+};
+Order.prototype.shuffleOrders = function () {
+        this.freeOrders = shuffle(this.availableOrders);
+    };
+Order.prototype.rate = function (sign) {
+        switch (sign) {
+            case '+':
+                this.pluses++;
+                break;
+            case '0':
+                this.zeros++;
+                break;
+            case '-':
+                this.minuses++;
+                break;
+            default:
+                this.zeros++;
+        }
+    };
+Order.prototype.refillOrders = function () {
+        var current, result=baseLetters.concat(), order;
+
+        for (var b=0;b<this.bit; b++) {
+            current = result.slice(0);
+            order=0;
+            for (var i=0; i<current.length;i++) {
+
+                for (var j=0;j<12;j++) {
+
+                    result[order++]=current[i]+baseLetters[j];
+                }
+            }
+        }
+        this.bit++;
+        this.availableOrders = result;
+        this.shuffleOrders();
+    };
+Order.prototype.say = function (order, type, picLink, text) {
+    var place = this.availableOrders.indexOf(order);
+    if (place>=0) {
+        var time = new Date();
+        this.sayings.push({order:order, type:type, picLink:picLink, date:time.toLocaleDateString(), time:time.toLocaleTimeString(),  text:text});
+        this[order]=new Order(order,type,picLink,text);
+        this.availableOrders.splice(place,1);
+        this.pretaken='';
+        this.shuffleOrders();
+        $scope.linkedText = '';   //view repair
+        if (this.availableOrders.length == 0) {this.refillOrders()};
+        return true;
+    }
+    return false;
+};
+Order.prototype.returnOrder = function () {
+    this.pretaken='';
+    this.shuffleOrders();
+};
+Order.prototype.pretakeOrder = function (order) {
+    this.pretaken = order;
+    this.shuffleOrders();
+    return order
+};
 
     $scope.root = new Order();
     $scope.getColor = function (order) {
@@ -172,11 +177,11 @@ $scope.firebase = $firebase(new Firebase('https://frukt.firebaseio.com')); //cre
 $scope.firebase.$bind($scope, "remote"); //bind a $scope.remoteLetters object for saving
 
 $scope.saveToFireBase = function () {
- $scope.remote.root = $scope.root.exportSayings();
+    $scope.remote.root = $scope.root.exportSayings();
 };
 $scope.loadFromFireBase = function () {
-$scope.root=new Order();
-$scope.root.importSayings($scope.remote.root);
+    $scope.root=new Order();
+    $scope.root.importSayings($scope.remote.root);
     $scope.root.shuffleOrders();
  };
 
